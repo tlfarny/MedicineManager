7af7bf520f588b9c0770ee13b507e09bcdd86e4f
Travis Farnsworth <travis.l.farnsworth@gmail.com>
2017-04-30 05:40:09 -0600

    Initial Commit

---
+++ MedicineManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedicineManager
+//
+//  Created by Travis Farnsworth on 4/30/17.
+//  Copyright © 2017 Travis Farnsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
